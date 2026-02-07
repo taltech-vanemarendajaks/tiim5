2c3619c3f3511eb5666be78296dff8e6f83670dc
Mari Piiriste <mpiiri@taltech.ee>
2026-02-07 21:34:17 +0200

    add spring and gradle configuration

---
+++ backend/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.tiim5"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ backend/src/main/java/controller/DemoApplicationController.java
@@ -0,0 +1,13 @@
+package com.tiim5.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoApplicationController {
+     @GetMapping("/hello")
+    public String hello(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello " + name + "!";
+    }
+}
