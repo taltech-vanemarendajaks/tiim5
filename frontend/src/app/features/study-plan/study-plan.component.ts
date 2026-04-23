ee5bcaddae24e9b4c10562651132ea5e9f83ff05
KaisaK1 <kaisakarjus12@gmail.com>
2026-04-23 21:42:53 +0300

    #53 refactor

--- frontend/src/app/features/study-plan/study-plan.component.ts
+++ frontend/src/app/features/study-plan/study-plan.component.ts
@@ -1,13 +1,33 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, signal, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CoursesComponent } from '../courses/courses.component';
+import { NotificationComponent, SearchComponent, SemesterBadgeComponent } from '@/components';
+import { CourseControllerService, CourseResponse } from '../../../client/_generated_';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { filter, switchMap } from 'rxjs/operators';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-study-plan',
   standalone: true,
-  imports: [CommonModule, CoursesComponent],
+  imports: [
+    CommonModule,
+    NotificationComponent,
+    SearchComponent,
+    SemesterBadgeComponent,
+  ],
   templateUrl: './study-plan.component.html',
   styleUrl: './study-plan.component.css',
 })
-export class StudyPlanComponent {}
+export class StudyPlanComponent {
+  private readonly courseService = inject(CourseControllerService);
+
+  readonly searchTitle = signal<string>('');
+
+  readonly courses: Signal<CourseResponse[]> = toSignal(
+    toObservable(this.searchTitle).pipe(
+      filter((title) => title.length === 0 || title.length > 2),
+      switchMap((title) => this.courseService.getAllCourses(undefined, 10, title)),
+    ),
+    { initialValue: [] },
+  );
+}
