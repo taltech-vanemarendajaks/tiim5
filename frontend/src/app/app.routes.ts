69288c98d5d85728ce09bbadfa25b8b7e8b8e38f
KaisaK1 <kaisakarjus12@gmail.com>
2026-04-24 10:56:45 +0300

    #53 refactor after merge

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,13 +9,11 @@ export const routes: Routes = [
       { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
       {
         path: 'dashboard',
-        loadComponent: () =>
-          import('./components/dashboard/dashboard').then((m) => m.Dashboard),
+        loadComponent: () => import('./components/dashboard/dashboard').then((m) => m.Dashboard),
       },
       {
         path: 'studies',
-        loadComponent: () =>
-          import('./components/studies/studies').then((m) => m.Studies),
+        loadComponent: () => import('./components/studies/studies').then((m) => m.Studies),
       },
       {
         path: 'curriculums',
@@ -28,21 +26,19 @@ export const routes: Routes = [
           {
             path: 'new',
             loadComponent: () =>
-              import(
-                './components/curriculums/curriculum-planning/curriculum-planning'
-              ).then((m) => m.CurriculumPlanning),
+              import('./components/curriculums/curriculum-planning/curriculum-planning').then(
+                (m) => m.CurriculumPlanning,
+              ),
           },
         ],
       },
       {
         path: 'schedule',
-        loadComponent: () =>
-          import('./components/schedule/schedule').then((m) => m.Schedule),
+        loadComponent: () => import('./components/schedule/schedule').then((m) => m.Schedule),
       },
       {
         path: 'settings',
-        loadComponent: () =>
-          import('./components/settings/settings').then((m) => m.Settings),
+        loadComponent: () => import('./components/settings/settings').then((m) => m.Settings),
       },
       { path: '**', redirectTo: 'dashboard' },
     ],
--- frontend/src/app/components/curriculums/curriculum-planning/curriculum-planning.ts
+++ frontend/src/app/components/curriculums/curriculum-planning/curriculum-planning.ts
@@ -1,11 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { TPipe } from '../../../pipes/t.pipe';
+import { CommonModule } from '@angular/common';
+import { NotificationComponent, SearchComponent, SemesterBadgeComponent } from '@/components';
+import { CourseService } from '@/client';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-curriculum-planning',
-  imports: [TPipe],
+  imports: [TPipe, CommonModule, NotificationComponent, SearchComponent, SemesterBadgeComponent],
   templateUrl: './curriculum-planning.html',
   styleUrl: './curriculum-planning.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CurriculumPlanning {}
+export class CurriculumPlanning {
+  private readonly courseService = inject(CourseService);
+
+  readonly searchTitle = signal<string>('');
+  readonly searchTitle$ = toObservable(this.searchTitle);
+
+  readonly courses$ = this.searchTitle$.pipe(
+    filter((title) => title.length === 0 || title.length > 2),
+    switchMap((title) => this.courseService.getAllCourses(undefined, 10, title)),
+  );
+
+  readonly courses = toSignal(this.courses$, { initialValue: [] });
+}
