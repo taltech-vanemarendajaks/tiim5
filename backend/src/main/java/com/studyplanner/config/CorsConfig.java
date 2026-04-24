d0eb6e9e7812505623ecc89c63f334f470db366b
KaisaK1 <kaisakarjus12@gmail.com>
2026-04-24 12:00:59 +0300

    #53 fix cors

--- backend/src/main/java/com/studyplanner/config/CorsConfig.java
+++ backend/src/main/java/com/studyplanner/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.studyplanner.config;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,11 +12,11 @@ public class CorsConfig {
   public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurer() {
       @Override
-      public void addCorsMappings(CorsRegistry registry) {
+      public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry
             .addMapping("/api/**")
-            .allowedOrigins("http://localhost:4200")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            .allowedOrigins("http://localhost:4200", "https://studyplanner.ddns.net")
+            .allowedMethods("*");
       }
     };
   }
