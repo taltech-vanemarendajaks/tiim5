86b944bb14225408a91435a8b7493606d15789c7
KaisaK1 <kaisakarjus12@gmail.com>
2026-04-26 19:39:12 +0300

    #58 fix openapi

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { OpenAPI } from '@/client';
+
+OpenAPI.BASE = '';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
