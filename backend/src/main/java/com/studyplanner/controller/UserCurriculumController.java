79be935222aeaa03bd595620be7868731757871a
Elina Liibak <elliib@taltech.ee>
2026-04-28 20:05:50 +0300

    imports

--- backend/src/main/java/com/studyplanner/controller/UserCurriculumController.java
+++ backend/src/main/java/com/studyplanner/controller/UserCurriculumController.java
@@ -10,10 +10,7 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Tag(name = "Curriculum")
